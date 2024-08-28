7cd25bcb2cd98a99815a631b24ab1f01f11d7b9a
harshpreet931 <harshpreet0402@gmail.com>
2024-08-28 10:38:57 +0530

    Time function Calculation Added

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #define TRAIN_RATIO 0.8
 
 int main() {
+    struct timeval start, end;
     char (*emails)[MAX_EMAIL_SIZE] = malloc(MAX_EMAILS * sizeof(*emails));
     int *labels = malloc(MAX_EMAILS * sizeof(*labels));
     
@@ -65,8 +66,16 @@ int main() {
     
     printf("Training on %d emails, testing on %d emails\n", train_count, test_count);
     
+    gettimeofday(&start, NULL);
+    long long before = start.tv_sec * 1000LL + start.tv_usec / 1000;
+
     train(emails, labels, train_count);
     test(emails + train_count, labels + train_count, test_count);
+
+    gettimeofday(&end, NULL);
+    long long after = end.tv_sec * 1000LL + end.tv_usec / 1000;
+
+    printf("Time taken: %lld ms\n", after - before);
 
     free(emails);
     free(labels);
