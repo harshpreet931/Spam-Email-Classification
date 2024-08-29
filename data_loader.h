8c36a0727db5230bf70b4f4e350c526f1f0d80dc
harshpreet931 <harshpreet0402@gmail.com>
2024-08-29 13:38:18 +0530

    Issue Fixed, Working model again.

--- data_loader.h
+++ data_loader.h
@@ -1,5 +1,10 @@
+#ifndef DATA_LOADER_H
+#define DATA_LOADER_H
+
 #include "spam_classifier.h"
 
 #define MAX_EMAILS 10000
 
 int load_data(char (*emails)[MAX_EMAIL_SIZE], int *labels, char *filename);
+
+#endif
--- spam_classifier.h
+++ spam_classifier.h
@@ -1,3 +1,6 @@
+#ifndef SPAM_CLASSIFIER_H
+#define SPAM_CLASSIFIER_H
+
 #define MAX_EMAIL_SIZE 1000
 #define MAX_WORDS 10000
 #define MAX_TOKENS 1000
@@ -13,3 +16,5 @@ typedef struct
 void train(char emails[][MAX_EMAIL_SIZE], int labels[], int email_count);
 int predict(char *email);
 void test(char test_emails[][MAX_EMAIL_SIZE], int test_labels[], int test_count);
+
+#endif
