fa23610166c3b5ea26fec056ce4625c4ae365573
harshpreet931 <harshpreet0402@gmail.com>
2024-08-30 11:24:39 +0530

    Added 2 Balanced Datasets to Test

--- main.c
+++ main.c
@@ -9,52 +9,7 @@
 #define MAX_EMAILS 10000
 #define TRAIN_RATIO 0.80
 
-int main(int argc, char *argv[]) {
-    if(argc > 1 && strcmp(argv[1], "-p") == 0) {
-        struct timeval start, end;
-        char (*emails)[MAX_EMAIL_SIZE] = malloc(MAX_EMAILS * sizeof(*emails));
-        int *labels = malloc(MAX_EMAILS * sizeof(*labels));
-        int email_count = load_data(emails, labels, "./spm_dataset.txt");
-        int train_count = (int)(email_count * TRAIN_RATIO);
-        int test_count = email_count - train_count;
-        
-        gettimeofday(&start, NULL);
-        long long before = start.tv_sec * 1000LL + start.tv_usec / 1000;
-
-        train(emails, labels, train_count);
-        test(emails + train_count, labels + train_count, test_count);
-
-        gettimeofday(&end, NULL);
-        long long after = end.tv_sec * 1000LL + end.tv_usec / 1000;
-        printf("Time taken: %lld ms\n", after - before);
-        // email starts
-        char email[MAX_EMAIL_SIZE];
-        printf("Enter the email content: (Press Ctrl+D to finish)\n");
-        int input_length = 0;
-        char c;
-        while ((c = getchar()) != EOF && input_length < MAX_EMAIL_SIZE - 1) {
-            if(c == '|') break; 
-            
-            email[input_length++] = c;
-        }
-        email[input_length] = '\0';
-
-        int prediction = predict(email);
-        printf("Prediction: %d", prediction);
-        if(prediction == 1) {
-            printf("The email is spam\n");
-        }
-        else {
-            printf("The email is not spam\n");
-        }
-        printf("%lld", calculate_probability(email, 1));
-        printf("%lld", calculate_probability(email, 0));
-
-        free(emails);
-        free(labels);
-        return 0;
-    }
-    
+int main() {
     struct timeval start, end;
     char (*emails)[MAX_EMAIL_SIZE] = malloc(MAX_EMAILS * sizeof(*emails));
     int *labels = malloc(MAX_EMAILS * sizeof(*labels));
@@ -71,6 +26,26 @@ int main(int argc, char *argv[]) {
     gettimeofday(&end, NULL);
     long long after = end.tv_sec * 1000LL + end.tv_usec / 1000;
     printf("Time taken: %lld ms\n", after - before);
+
+    printf("Enter an email to predict: (To stop enter - '|')\n");
+    char email[MAX_EMAIL_SIZE];
+    while(1)
+    {
+        fgets(email, MAX_EMAIL_SIZE, stdin);
+        if(email[0] == '|')
+        {
+            break;
+        }
+        int prediction = predict(email);
+        if(prediction == 1)
+        {
+            printf("Spam\n");
+        }
+        else
+        {
+            printf("Not Spam\n");
+        }
+    }
 
     free(emails);
     free(labels);
