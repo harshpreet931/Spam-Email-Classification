f4c09ea0de7d21fc44a1e0af197584b0b862d17f
harshpreet931 <harshpreet0402@gmail.com>
2024-09-03 15:15:44 +0530

    Updated shell file

--- run_project.sh
+++ run_project.sh
@@ -1,12 +1,15 @@
 gcc -c data_loader.c -o data_loader
 gcc -c spam_classifier_impl.c -o spam_classifier_impl
+gcc -c model_io.c -o model_io
 
-ar rcs lib.a data_loader spam_classifier_impl
+ar rcs lib.a data_loader spam_classifier_impl model_io
 
 gcc -o test_output main.c -L. lib.a
 
 if [ $? -eq 0 ]; then
-    ./test_output
+    echo "Compilation successful"
+    echo "./test_output to run the program"
+    echo "./test_output --predict to run the program in predict mode"
 else 
     echo "Compilation failed"
 fi
