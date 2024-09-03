4105772dbf1f8b97588451424b844b9320168a5d
harshpreet931 <harshpreet0402@gmail.com>
2024-09-03 14:50:52 +0530

    Added function to save the model

--- model_io.c
+++ model_io.c
@@ -1,3 +1,16 @@
 #include "model_io.h"
 #include <stdio.h>
 
+void save_model(char* filename, WordProbability* word_probs, int word_prob_count) {
+    File *file = fopen(filename, "wb");
+    if (file == NULL) {
+        printf("Error opening file to write %s\n", filename);
+        return 0;
+    }
+
+    fwrite(&word_prob_count, sizeof(int), 1, file);
+    fwrite(word_probs, sizeof(WordProbability), word_prob_count, file);
+
+    fclose(file);
+    printf("Model saved to %s\n", filename);
+}
