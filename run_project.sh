4f89e60598834a6ef340065591ab9ed34949a140
harshpreet931 <harshpreet0402@gmail.com>
2024-09-04 14:43:28 +0530

    Refactor run_project.sh

--- run_project.sh
+++ run_project.sh
@@ -1,11 +1,15 @@
+# To compile all the source files that contain the implementation of the header files.
 gcc -c data_loader.c -o data_loader
 gcc -c spam_classifier_impl.c -o spam_classifier_impl
 gcc -c model_io.c -o model_io
 
+# To create a static library
 ar rcs lib.a data_loader spam_classifier_impl model_io
 
+# To compile the main file
 gcc -o test_output main.c -L. lib.a
 
+# To check if the compilation was successful
 if [ $? -eq 0 ]; then
     echo "Compilation successful"
     echo "./test_output to run the program"
