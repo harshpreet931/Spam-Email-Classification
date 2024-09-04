390fcf7d32ed29e742e036a846cdd77b3a31cefd
harshpreet931 <harshpreet0402@gmail.com>
2024-09-04 16:09:40 +0530

    Refactoring main.c, Today's entire day has been used to refactor my dirty code lol!

--- main.c
+++ main.c
@@ -11,52 +11,84 @@
 #define TRAIN_RATIO 0.80
 #define MODEL_FILENAME "spam_model.bin"
 
+// External variables defined in spam_classifier_impl.c
 extern WordProbability word_probs[MAX_WORDS];
 extern int word_prob_count;
 
-int main(int argc, char *argv[]) {
-    if(argc > 1 && (strcmp(argv[1], "--predict") == 0)) {
+int main(int argc, char *argv[]) 
+{
+    // Check if the program is run with the "--predict" argument
+    if(argc > 1 && (strcmp(argv[1], "--predict") == 0)) 
+    {
+        // Load the pre-trained model from the file
         word_prob_count = load_model(MODEL_FILENAME, word_probs, MAX_WORDS);
-        if (word_prob_count < 0) {
+        if (word_prob_count < 0) 
+        {
             printf("Error loading model\n");
             return 1;
         }
 
         printf("Enter email to predict:  (To stop press -> '|')\n");
         char email[MAX_EMAIL_SIZE];
-        while(1) {
+        // Continuously read emails from the user and predict if they are spam or not
+        while(1) 
+        {
             fgets(email, MAX_EMAIL_SIZE, stdin);
-            if (email[0] == '|') {
+            // Stop if the user enters '|'
+            if (email[0] == '|') 
+            {
                 break;
             }
+
+            // Predict if the email is spam or not
             int prediction = predict(email);
-            if (prediction == 1) {
+            if (prediction == 1) 
+            {
                 printf("SPAM\n");
-            } else {
+            } else 
+            {
                 printf("NOT SPAM\n");
             }
         }
     }
-    else {
+    else 
+    {
+        // Normal Training and Testing mode, Default mode.
+
+        // Timing variables for measuring performance
         struct timeval start, end;
+
+        // Allocate memory for emails and labels
         char (*emails)[MAX_EMAIL_SIZE] = malloc(MAX_EMAILS * sizeof(*emails));
         int *labels = malloc(MAX_EMAILS * sizeof(*labels));
+
+        // Load email data and the labels from the dataset
         int email_count = load_data(emails, labels, "./spm_database.txt");
+
+        // Calculate the number of emails for training and testing
         int train_count = (int)(email_count * TRAIN_RATIO);
         int test_count = email_count - train_count;
         
+        // Start the timer before training and testing
         gettimeofday(&start, NULL);
         long long before = start.tv_sec * 1000LL + start.tv_usec / 1000;
 
+        // Train the model using the training data
         train(emails, labels, train_count);
+
+        // Test the model using the testing data
         test(emails + train_count, labels + train_count, test_count);
 
+        // Stop the timer after training and testing and print the performance
         gettimeofday(&end, NULL);
         long long after = end.tv_sec * 1000LL + end.tv_usec / 1000;
         printf("Time taken: %lld ms\n", after - before);
 
+
+        // Save the trained model to a file for future use
         save_model(MODEL_FILENAME, word_probs, word_prob_count);
 
+        // Free the allocated memory!
         free(emails);
         free(labels);
     }
