d282444b6ad6daf26ac8390e63934c4d5cc621f7
harshpreet931 <harshpreet0402@gmail.com>
2024-09-04 14:56:58 +0530

    Refactor data_loader.c

--- data_loader.c
+++ data_loader.c
@@ -3,9 +3,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-int load_data(char (*emails)[MAX_EMAIL_SIZE], int *labels, char *filename) {
+// Loads email data and their labels from a txt file provided.
+// Returns the number of emails loaded from the file.
+int load_data(char (*emails)[MAX_EMAIL_SIZE], int *labels, char *filename) 
+{
     FILE* file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL) 
+    {
+        // Error opening file, return -1
         printf("Error opening file %s\n", filename);
         return -1;
     }
@@ -14,26 +19,36 @@ int load_data(char (*emails)[MAX_EMAIL_SIZE], int *labels, char *filename) {
     int email_count = 0;
     char line[MAX_EMAIL_SIZE + 10];
 
-    while (fgets(line, sizeof(line), file) && email_count < MAX_EMAILS) {
+    // Read each line from the file until the end or the maximum number of emails
+    while (fgets(line, sizeof(line), file) && email_count < MAX_EMAILS) 
+    {
+        // Tokenize the line to get the label and the email content
         char *label_str = strtok(line, ",");
         char *content = strtok(NULL, "\n");
 
-        if (label_str && content) {
+        // If both label and content are present, process and store them.
+        if (label_str && content) 
+        {
             labels[email_count] = atoi(label_str);
             strncpy(emails[email_count], content, MAX_EMAIL_SIZE - 1);
             emails[email_count][MAX_EMAIL_SIZE - 1] = '\0';
             email_count++;
         }
         
-        if (email_count % 1000 == 0) {
+        // Verbose output.
+        if (email_count % 1000 == 0) 
+        {
             printf("Processed %d emails\n", email_count);
         }
     }
     fclose(file);
 
+    // Verbose output.
     printf("Total emails processed: %d\n", email_count);
 
-    if (email_count == 0) {
+    // If no emails were read, return -1
+    if (email_count == 0) 
+    {
         printf("No emails were read from the file. Check the file content.\n");
         return -1;
     }
